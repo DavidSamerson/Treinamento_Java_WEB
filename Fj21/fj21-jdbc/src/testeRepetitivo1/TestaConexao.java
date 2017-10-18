5a05f550a397c9b4db1f47fa19ec4f27e503e845
David Samerson <samerson10@gmail.com>
2017-10-18 17:39:48 -0300

    novo

---
+++ Fj21/fj21-jdbc/src/testeRepetitivo1/TestaConexao.java
@@ -0,0 +1,12 @@
+package testeRepetitivo1;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class TestaConexao {
+	public static void main(String[] args) throws SQLException {
+		Connection con = new ConnectionFactory().getConnection();	
+		System.out.println("Conexão Estabelecida com sucesso");
+		con.close();
+	}
+}
